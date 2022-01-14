28287e84e667aad34cac8a4d659c6cd9c608eff1
Im Yerim <96689423+yerm13@users.noreply.github.com>
2022-01-14 17:15:27 +0900

    Create 2588.py

---
+++ 2588.py
@@ -0,0 +1,12 @@
+first = int(input())
+second = input()
+
+third = first * int(second[2])
+fourth = first * int(second[1])
+fifth = first * int(second[0])
+sixth = first * int(second)
+
+print(third)
+print(fourth)
+print(fifth)
+print(sixth)
